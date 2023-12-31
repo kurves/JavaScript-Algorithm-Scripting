08318c499732882eca8f40a93ca00a7b372f2bc4
kurves <kavssimply@gmail.com>
2023-12-31 22:34:21 +0300

    loop throught hte provuded sentence

--- longestWord.js
+++ longestWord.js
@@ -1,6 +1,10 @@
 function longestWordLength(str){
-    str.split(' ')
+   let words= str.split(' ')
     console.log(str)
+
+    for(let i=0; i<str.length;i++){
+        console.log(str[i])
+    }
 
 }
 
