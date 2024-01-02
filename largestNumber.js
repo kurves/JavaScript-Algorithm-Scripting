c960830b9d05b9189682d17369f05cfe77b8a5b8
kurves <kavssimply@gmail.com>
2024-01-02 23:09:46 +0300

    correct for loop

--- largestNumber.js
+++ largestNumber.js
@@ -1,9 +1,12 @@
-function largestNumber(arr){
-    let largestArrNumber =0;
+function largestNumberOfArray(arr){
+    // declare largestArrNumbervariable
+    let largestArrNumber =[];
+    // for loop 
     for (let i =0; i < arr.length; i++){
-        for (let j =0 ; j < arr[i]; j++){
-            if (arr[i][j] > largestArrNumber){
-                largestArrNumber = arr[i][j]
+        let largestNumber = ar[i][0]
+        for (let j =1 ; j < arr[i].length; j++){
+            if (arr[i][j] > largestNumber){
+                largestNumber = arr[i][j]
             }
             console.log(largestArrNumber);
         }
@@ -12,4 +15,4 @@ function largestNumber(arr){
 
 }
 
-largestNumber([[1,6,7,8],[2,3,7,1],[8,9,6,4],[7,5,4,2]])
+largestNumberOfArray([[1,6,7,8],[2,3,7,1],[8,9,6,4],[7,5,4,2]])
