af405e95cfed349a28a44c1c1c5e33e8f67dfcee
kurves <kavssimply@gmail.com>
2024-01-02 23:12:23 +0300

    fix compilation error

--- largestNumber.js
+++ largestNumber.js
@@ -3,7 +3,7 @@ function largestNumberOfArray(arr){
     let largestArrNumber =[];
     // for loop 
     for (let i =0; i < arr.length; i++){
-        let largestNumber = ar[i][0]
+        let largestNumber = arr[i][0]
         for (let j =1 ; j < arr[i].length; j++){
             if (arr[i][j] > largestNumber){
                 largestNumber = arr[i][j]
