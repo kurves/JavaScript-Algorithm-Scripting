90f83fc6c16844f38284734aafacee94b7e635b6
kurves <kavssimply@gmail.com>
2024-01-03 22:52:59 +0300

    Fix if statements for correct output

--- confirmEnding.js
+++ confirmEnding.js
@@ -1,7 +1,12 @@
+// function to check for subctring
 function confirmEnding(str, target){
-if( str.substring() === target){
+
+if( str.substring(str.length -1) == target){
 return true
+
 }else {
     return false
+
 }
 }
+confirmEnding("bastian","n")
