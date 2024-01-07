8bf24b95bd94b4ff8399cd7c956fad32233968a6
kurves <kavssimply@gmail.com>
2024-01-07 23:14:10 +0300

    function to insert item at index

---
+++ insertItem.js
@@ -0,0 +1,12 @@
+function insertItem(arr, num){
+mid = arr.length // 2
+for (let i =0; i <arr.length; i++){
+    if (arr[i] < mid){
+        let left = arr[i]
+    }else{
+        if(arr[i] >mid){
+            let right = arr[i]
+        }
+    }
+}
+}
