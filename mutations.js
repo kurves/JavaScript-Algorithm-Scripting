55bcb556443a8e357c0a282827fe92915130e4f0
kurves <kavssimply@gmail.com>
2024-01-09 22:32:06 +0300

    function to get the mutation of string

--- mutations.js
+++ mutations.js
@@ -2,7 +2,13 @@ function mutations(arr){
     //Convert individual words to lowercase
     const firstWord = arr[0].toLowerCase()
     const secondWord = arr[1].toLowerCase()
-if (arr[0] == arr[1]){
-    console.log("true")
+    //loop through
+     for (let i =0; i<arr.lenth; i++){
+        
+if (firstWord.indexOf(secondWord[i] < 0)){
+    return false
+}else{
+    return true
+}
 }
 }
