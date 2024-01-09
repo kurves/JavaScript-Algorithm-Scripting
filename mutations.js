0770a0e5e662e64c825bde5d409bd74ff8176768
kurves <kavssimply@gmail.com>
2024-01-09 22:28:16 +0300

    convert words to lowercase for comparison

--- mutations.js
+++ mutations.js
@@ -1,4 +1,7 @@
 function mutations(arr){
+    //Convert individual words to lowercase
+    const firstWord = arr[0].toLowerCase()
+    const secondWord = arr[1].toLowerCase()
 if (arr[0] == arr[1]){
     console.log("true")
 }
