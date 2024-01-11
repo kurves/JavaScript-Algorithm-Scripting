9e14642d1ac0ddfe0cf15dc78f3a0da6636c6ba6
kurves <kavssimply@gmail.com>
2024-01-11 20:39:23 +0300

    function to get sum inbetween

--- sumRange.js
+++ sumRange.js
@@ -1,7 +1,10 @@
 function sumRange(arr){
     let max = Math.max(arr[0], arr[1])
     let min = Math.min(arr[0],arr[1])
-for(let i=0; i<arr.length;i++){
-    sum = arr[i]
+    let sum =0
+
+for(let i=min; i<= max;i++){
+    sum += i
 }
+return sum
 }
