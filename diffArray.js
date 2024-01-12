90ff5ae293859dd39647c14cbdba2f476a2ad701
kurves <kavssimply@gmail.com>
2024-01-12 21:05:08 +0300

    second function to get items only in first array

--- diffArray.js
+++ diffArray.js
@@ -1,11 +1,13 @@
 function diffArray(arr1,arr2){
 const newArr=[];
-for (let i=0;i < arr1.length; i++){
-    for(let j=0; j<arr2.length;j++){
+function inFirst(first,second){
+    for (let i=0;i < arr1.length; i++){
         if (i != j){
             newArr.push(i)
         }
     }
-    return newArr
+    console.log(newArr)
 }
+
 }
+diffArray([1,4,5,7,8],[6,7,9,0,4])
