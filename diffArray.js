a2a926914749734eb3fb22fcddaf3bd75cc20d2c
kurves <kavssimply@gmail.com>
2024-01-12 21:00:03 +0300

    Code to get array difference

--- diffArray.js
+++ diffArray.js
@@ -2,7 +2,10 @@ function diffArray(arr1,arr2){
 const newArr=[];
 for (let i=0;i < arr1.length; i++){
     for(let j=0; j<arr2.length;j++){
-        
+        if (i != j){
+            newArr.push(i)
+        }
     }
+    return newArr
 }
 }
