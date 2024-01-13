8c9b6cad5169f4beab67afd87bcbc525f2289ce2
kurves <kavssimply@gmail.com>
2024-01-13 20:09:24 +0300

    fucntion to delete elements

--- destroyer.js
+++ destroyer.js
@@ -1,5 +1,12 @@
 function destroyer(arr){
 const valRemove = Object.values(arguments).slice(1)
 const filtered =[]
-
+for (let i=0;i < arr.length;i++){
+    let removeElem = false;
+    for (let j=0;j< valRemove.length;j++){
+        if(arr[i] === valRemove[j]){
+            removeElem = true;
+        }
+    }
+}
 }
