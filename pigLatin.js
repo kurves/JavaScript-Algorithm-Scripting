419ad71362f0aa3c256a07e8c20acd4989bafbb1
kurves <kavssimply@gmail.com>
2024-01-16 20:55:50 +0300

    function to get pig latin

--- pigLatin.js
+++ pigLatin.js
@@ -1,7 +1,10 @@
 //altering enlish words
 function pigLatin(str){
-for( let i=0; i<= str.length;i++){
-    console.log(str[i])
+for( let i=0; i< str.length;i++){
+    if (ord('a') <= ord(str[i]) <= ord('z')){
+        
+    }
 }
 
 }
+pigLatin("maureen")
