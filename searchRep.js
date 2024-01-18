d736b0d6c14064efad2a3ae37ad8da6f347f9659
kurves <kavssimply@gmail.com>
2024-01-18 20:23:48 +0300

    function o search and replace

--- searchRep.js
+++ searchRep.js
@@ -2,4 +2,6 @@ function searchReplace(str, before, after){
     const strArr = arr.spilt(" ")
     const wordReplace = strArr.filter(item=> item === before);
     const rep = wordReplace[0] === wordReplace.toUpperCase ? after[0].toUpperCase() + after.slice(1)
+    : after[0].toLowerCase() + after.slice(1);
+    return strArr.map(item =>(item === before ? replace: item))
 }
