7fd757ca598dd1e3c1230b5d1ea288c316a1f1c2
kurves <kavssimply@gmail.com>
2024-01-18 20:21:12 +0300

    function to search and replace

---
+++ searchRep.js
@@ -0,0 +1,5 @@
+function searchReplace(str, before, after){
+    const strArr = arr.spilt(" ")
+    const wordReplace = strArr.filter(item=> item === before);
+    const rep = wordReplace[0] === wordReplace.toUpperCase ? after[0].toUpperCase() + after.slice(1)
+}
