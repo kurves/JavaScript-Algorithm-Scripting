881949b7d37c2b70470659607d0b5e43a12eb4e1
kurves <kavssimply@gmail.com>
2024-01-20 19:59:28 +0300

    function to check missing letters

--- missingLetters.js
+++ missingLetters.js
@@ -1,8 +1,8 @@
 function missingLetters(str){
     firstLetter = str[0]
     lastLetter = str[-1]
-    for ( let i =ord(firstLetter); i < ord(lastletter); i++ ){
-        console.log(i)
+    for ( let i =ord(firstLetter); i < ord(lastLetter); i++ ){
+        console.log(str(i))
     }
 
 }
