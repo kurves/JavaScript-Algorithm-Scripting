38cd24e01dca13df040203be8d6c1601056f7196
kurves <kavssimply@gmail.com>
2024-01-21 22:10:26 +0300

    check if letter missing

--- missingLetters.js
+++ missingLetters.js
@@ -1,7 +1,11 @@
 function missingLetters(str){
     for ( let i =0; i< str.length; i++){
         const char =  str.charCodeAt(i);
-        console.log(str(i))
+        if (char !== str.charCodeAt(0) + i){
+            return String.fromCharCode(char -1)
+        }
+        
     }
+    return undefined;
 
 }
