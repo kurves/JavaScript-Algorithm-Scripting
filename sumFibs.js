e42130583bb7d172e5044e2092d5c09db6e86dc8
kurves <kavssimply@gmail.com>
2024-01-29 16:59:08 +0300

    fucntionn to add odd fib sums

--- sumFibs.js
+++ sumFibs.js
@@ -1,18 +1,14 @@
 function sumFibs(num){
     // function to sum oddufib nums]
-    let a =0
-    let b =1
-    fib_sum =0
-
-    while  (b<= num){
-        if (b% 2 ==0){
-            fib_sum += b
-        }
-      a ,b = b, a+b
-      print(fib_sum)
-
+  let a =0
+  let b =1
+  fib_sums = 0
+  while (b <= num){
+    if(num % 2 != 0){
+fib_sums += b
+a,b = b, a+b
     }
-
+  }
 
 }
 sumFibs(10)
