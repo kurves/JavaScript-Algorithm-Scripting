9e403e5bc86f3a720a23dcacebc8955e9368c618
kurves <kavssimply@gmail.com>
2024-02-01 17:11:50 +0300

    function to add primes

--- sumPrimes.js
+++ sumPrimes.js
@@ -2,7 +2,7 @@ function sumPrimes(num){
     //funtiion to sum primes
     let sum_prime =0;
     n = 1
-    prime= num % num == 0;
+    prime= (num % num == 0 and num% 1 == 0);
     for (let n = 1; n<= num ; n++){
 
     }
