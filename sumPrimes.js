7d75d1fdb801da7b88295820954f6a97aa234c72
kurves <kavssimply@gmail.com>
2024-02-01 17:14:18 +0300

    function to add primes in range

--- sumPrimes.js
+++ sumPrimes.js
@@ -4,13 +4,13 @@ function sumPrimes(num){
     n = 1
     prime= (num % num == 0 && num% 1 == 0);
     for (let n = 1; n<= num ; n++){
-        if (prime){
-            sum_prime += num
-        }
+        while (n<= num){
+            if (prime){
+                sum_prime += num
+            }
+
+        }  }
 
     }
-    while ( n <=num ){
+   
 
-    }
-
-}
