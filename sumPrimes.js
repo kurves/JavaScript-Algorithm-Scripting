ff3702a1cb808d872f7bee13785b7bd7bea634be
kurves <kavssimply@gmail.com>
2024-02-02 17:25:08 +0300

    function to get sum of primes

--- sumPrimes.js
+++ sumPrimes.js
@@ -4,6 +4,12 @@ function sumPrimes(num){
     n = 1
     prime= (num % num == 0 && num% 1 == 0);
     for (let n = 1; n<= num ; n++){
+
+        for (let i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i === 0) {
+                prime = false;
+                break;
+            }
         while (n<= num){
             if (prime){
                 sum_prime += num
