a9f7b12c0e866135e9b7cdb07c9bfbea0d10518c
kurves <kavssimply@gmail.com>
2024-02-12 20:02:28 +0300

    function to smallest common multiples

--- smallestCommonMultiple.js
+++ smallestCommonMultiple.js
@@ -3,6 +3,9 @@ function smallestCommonMultiple(arr){
     //sort arr
     arr.sort((a,b)=> a -b)
      let mult = arr[1]
+     while(isNotDivisible(mult, arr[0], arr[1])){
+mult += arr[1]
+     }
         while (y !== 0) {
             let temp = y;
             y = x % y;
