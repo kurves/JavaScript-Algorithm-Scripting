3f2d4ad0c3a2cc0ce0e68507a0550304fdbd8c94
kurves <kavssimply@gmail.com>
2024-02-12 19:58:13 +0300

    function to get the smallest common multiple

--- smallestCommonMultiple.js
+++ smallestCommonMultiple.js
@@ -12,11 +12,5 @@ function smallestCommonMultiple(arr){
     return (a * b) / gcd(a, b);
 }
 
-    start = arr[0];
-    end = arr[-1]
-    for(let i =start; i <=end; i++){
-        console.log(i)
-    }
-
-}
+   
 smallestCommonMultiple([1,5])
