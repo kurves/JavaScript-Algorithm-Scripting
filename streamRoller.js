e23309dfa2c4227e45d19f870bebca27aa599f44
kurves <kavssimply@gmail.com>
2024-03-14 23:56:27 +0300

    function to flatten nested array

--- streamRoller.js
+++ streamRoller.js
@@ -3,4 +3,11 @@
         arr.forEach(element => {
             if (Array.isArray(element)) {
                 result = result.concat(flattenArray(element));
-    }
+
+    }    } else {
+            result.push(element);
+        }
+    });
+
+    return result;
+}
