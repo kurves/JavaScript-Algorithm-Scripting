5a2f9076dc30898a52c60a5d24f1cbb0983ccbc4
kurves <kavssimply@gmail.com>
2024-04-02 22:56:15 +0300

    function to convertbinary to string

--- binaryAgents.js
+++ binaryAgents.js
@@ -2,6 +2,8 @@ function binaryAgents(binaryStr){
 
 const binaryDigits = binaryStr.split('');
 
+const charArray = binaryDigits.map(binaryStr)
+
 let translatedString = '';
 for (let i =0; i < binaryDigits;  i++){
         const decimalVal = parseInt(binaryDigits[1], 2)
