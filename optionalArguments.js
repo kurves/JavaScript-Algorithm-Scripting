1534fcb3c7a8f37fb0f6193ba78fd83f4aaa5841
kurves <kavssimply@gmail.com>
2024-04-18 09:02:44 +0300

    progrsm to add optional arguments

--- optionalArguments.js
+++ optionalArguments.js
@@ -8,4 +8,15 @@ function addTogether() {
           if (args.length === 1) {
             const num1 = args[0];
             return function(num2) {
+
+if (typeof num2 === 'number') {
+          return num1 + num2;
+        } else {
+          return undefined;
+        }
+      };
+    }
+  }
+  // If any argument is not a number, return undefined
+  return undefined;
 }
