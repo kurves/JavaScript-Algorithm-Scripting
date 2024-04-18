da1c4e62c41c11f00cff37a23f6afcc39bd05eee
kurves <kavssimply@gmail.com>
2024-04-18 09:01:27 +0300

    retutn funtion if one argument

--- optionalArguments.js
+++ optionalArguments.js
@@ -4,4 +4,8 @@ function addTogether() {
    
         if (args.length === 2) {
           return args[0] + args[1];
+
+          if (args.length === 1) {
+            const num1 = args[0];
+            return function(num2) {
 }
