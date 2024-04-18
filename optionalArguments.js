794fd5a4021e1bda5d82994cc8f70e9d774a6080
kurves <kavssimply@gmail.com>
2024-04-18 09:00:31 +0300

    check type of number

--- optionalArguments.js
+++ optionalArguments.js
@@ -1,3 +1,7 @@
 function addTogether() {
     const args = [...arguments];
+    if (args.every(arg => typeof arg === 'number')) {
+   
+        if (args.length === 2) {
+          return args[0] + args[1];
 }
